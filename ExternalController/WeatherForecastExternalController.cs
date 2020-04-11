b665ba95e033758f4baeeeba77eae62927f2fa8a
Prasad Patil <prasad.patil@gmail.com>
2020-04-12 00:02:35 +0100

    ExternalController example

---
+++ ExternalController/WeatherForecastExternalController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExternalController
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastExternalController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastExternalController> _logger;
+
+        public WeatherForecastExternalController(ILogger<WeatherForecastExternalController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
